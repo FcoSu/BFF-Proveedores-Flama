b6ee84802714268f57a92a1643422959f6322d7d
Francisco <panchoseur@gmail.com>
2021-03-30 11:42:16 -0300

    first commit

---
+++ src/main/java/com/everis/latam/BFFProveedoresFlama/BffProveedoresFlamaApplication.java
@@ -0,0 +1,13 @@
+package com.everis.latam.BFFProveedoresFlama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffProveedoresFlamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffProveedoresFlamaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everis/latam/BFFProveedoresFlama/BffProveedoresFlamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.latam.BFFProveedoresFlama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffProveedoresFlamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
